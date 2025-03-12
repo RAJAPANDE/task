83a9d1ee509bcfa6a2a5280eb6627db0006628df
Raj Kumar <129819252+RAJAPANDE@users.noreply.github.com>
2025-03-12 17:20:36 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,6 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const firebase = require('firebase-admin');
-const serviceAccount = require('./config/serviceAccountKey.json');
 
 // Middleware
 app.use(cors());
@@ -11,8 +10,19 @@ app.use(bodyParser.json());
 
 // Firebase Admin SDK initialization
 firebase.initializeApp({
-  credential: firebase.credential.cert(serviceAccount),
-   databaseURL: "https://to-do-list-98b94-default-rtdb.firebaseio.com" 
+  credential: firebase.credential.cert({
+    "type": "service_account",
+    "project_id": process.env.FIREBASE_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_AUTH_URI,
+    "token_uri": process.env.FIREBASE_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_CLIENT_X509_CERT_URL
+  }),
+  databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
 // Routes
